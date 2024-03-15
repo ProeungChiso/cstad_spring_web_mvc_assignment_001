9c47f2b026a53206d09113d1d1d350cbd6839281
Proeung Chiso <122223308+ProeungChiso@users.noreply.github.com>
2024-03-15 11:48:47 +0700

    starting project

---
+++ src/main/java/io/cstad/todolist_with_spring_web_mvc/TodolistWithSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package io.cstad.todolist_with_spring_web_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistWithSpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistWithSpringWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cstad/todolist_with_spring_web_mvc/TodolistWithSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cstad.todolist_with_spring_web_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistWithSpringWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
